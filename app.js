c044af6793a0bfbd642900e50cd438dbe2b74225
Tyler-Gee <tjgee1996@gmail.com>
2020-10-03 22:27:32 -0400

    implementing username validation for symbols or white space

--- app.js
+++ app.js
@@ -8,7 +8,69 @@ var Schema = mongoose.Schema;
 
 app.use(fileUpload());
 
-mongoose.connect("mongodb://localhost:2717/subscription-manager--users", { useNewUrlParser: true, useUnifiedTopology: true });
+// Minimization
+const fs = require("fs");
+const JavaScriptObfuscator = require("javascript-obfuscator");
+
+// Important, pass in port as in `npm run dev 1234`, do not change
+const portNum = process.argv[2];
+
+// Send HTML at root, do not change
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+// Send Style, do not change
+app.get("/style.css", function (req, res) {
+    //Feel free to change the contents of style.css to prettify your Web app
+    res.sendFile(path.join(__dirname + "/public/css/style.css"));
+});
+
+// Send obfuscated JS, do not change
+app.get("/index.js", function(req, res) {
+	fs.readFile(path.join(__dirname + "/public/index.js"), "utf8", function(err, contents) {
+		const minimizedContents = JavaScriptObfuscator.obfuscate(contents, { compact: true, controlFlowFlattening: true });
+		res.contentType("application/javascript");
+		res.send(minimizedContents._obfuscatedCode);
+	});
+});
+
+app.use('/img', express.static(__dirname + '/public/img'));
+
+//Respond to POST requests that upload files to uploads/ directory
+app.post("/upload", function(req, res) {
+	if (!req.files) {
+		return res.status(400).send("No files were uploaded.");
+	}
+
+	let uploadFile = req.files.uploadFile;
+
+	// Use the mv() method to place the file somewhere on your server
+	uploadFile.mv("uploads/" + uploadFile.name, function(err) {
+		if (err) {
+			return res.status(500).send(err);
+		}
+		res.redirect("/");
+	});
+});
+
+//Respond to GET requests for files in the uploads/ directory
+app.get("/uploads/:name", function(req, res) {
+	fs.stat("uploads/" + req.params.name, function(err, stat) {
+		console.log(err);
+		if (err == null) {
+			res.sendFile(path.join(__dirname + "/uploads/" + req.params.name));
+		} else {
+			res.send("");
+		}
+	});
+});
+
+
+
+
+
+mongoose.connect("mongodb://localhost:2717/subscription-manager-database", { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection
 	.once("open", () => console.log("Connected"))
 	.on("error", (error) => {
@@ -16,182 +78,50 @@ mongoose.connection
 	}),
 	{ useUnifiedTopology: true };
 
-
 const usersCollectionSchema = new Schema({
-    user_email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    user_password: {type: String, min: 8, max: 64}
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: { type: String, min: 8, max: 64 },
 });
 
 const userAccountCredentials = mongoose.model("users", usersCollectionSchema);
 
-// Minimization
-const fs = require("fs");
-const JavaScriptObfuscator = require("javascript-obfuscator");
-
-// Important, pass in port as in `npm run dev 1234`, do not change
-const portNum = process.argv[2];
-
-app.get("/test", (req, res) => {
-	res.send("The app is working");
-});
-
-app.get("/users",function (req,res){
-    mongoose.model('users').find(function(err, users){
-        res.send(users);
-    })
-});
-
-app.get("/createUser/:user_email/:user_password", function(req, res){
-    const test = new userAccountCredentials({
-		user_email: req.params.user_email.substring(1),
-		user_password: req.params.user_password.substring(1),
+app.get("/users", function (req, res) {
+	mongoose.model("users").find(function (err, users) {
+		res.send(users);
 	});
-    test.save(function (err) {
+});
+
+app.get("/createUser/:username/:password", function (req, res) {
+	const test = new userAccountCredentials({
+		username: req.params.username.substring(1),
+		password: req.params.password.substring(1),
+	});
+	test.save(function (err) {
 		if (err) return handleError(err);
 		// saved!
 	});
-    res.send(`Email: ${req.params.user_email.substring(1)}\nPassword: ${req.params.user_password.substring(1)}`);
+	res.send(`Username: ${req.params.username.substring(1)}\nPassword: ${req.params.password.substring(1)}`);
 });
 
-// Send HTML at root, do not change
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
-});
 
-// Send Style, do not change
-app.get("/style.css", function (req, res) {
-    //Feel free to change the contents of style.css to prettify your Web app
-    res.sendFile(path.join(__dirname + "/public/css/style.css"));
-});
 
-// Send obfuscated JS, do not change
-app.get("/index.js", function(req, res) {
-	fs.readFile(path.join(__dirname + "/public/index.js"), "utf8", function(err, contents) {
-		const minimizedContents = JavaScriptObfuscator.obfuscate(contents, { compact: true, controlFlowFlattening: true });
-		res.contentType("application/javascript");
-		res.send(minimizedContents._obfuscatedCode);
-	});
-});
 
-app.use('/img', express.static(__dirname + '/public/img'));
 
-app.get("/signIn/:user_email/:user_password", function (req, res) {
-    var email = req.params.user_email;
-    var password = req.params.user_password
-    res.send(`Sucessfull Sign In\nEmail: ${email}\nPassword: ${password}`);
-});
 
-//Respond to POST requests that upload files to uploads/ directory
-app.post("/upload", function(req, res) {
-	if (!req.files) {
-		return res.status(400).send("No files were uploaded.");
-	}
 
-	let uploadFile = req.files.uploadFile;
 
-	// Use the mv() method to place the file somewhere on your server
-	uploadFile.mv("uploads/" + uploadFile.name, function(err) {
-		if (err) {
-			return res.status(500).send(err);
-		}
-		res.redirect("/");
-	});
-});
 
-//Respond to GET requests for files in the uploads/ directory
-app.get("/uploads/:name", function(req, res) {
-	fs.stat("uploads/" + req.params.name, function(err, stat) {
-		console.log(err);
-		if (err == null) {
-			res.sendFile(path.join(__dirname + "/uploads/" + req.params.name));
-		} else {
-			res.send("");
-		}
-	});
-});
+
+
+
+
+
 
 
 app.listen(portNum, () => {
 	console.log(`Running app at localhost: ${portNum}`);
 });
-
-
-
-
-
-
-
-/* 
-
-// Minimization
-const fs = require("fs");
-const JavaScriptObfuscator = require("javascript-obfuscator");
-
-// Important, pass in port as in `npm run dev 1234`, do not change
-const portNum = process.argv[2];
-
-// Send HTML at root, do not change
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-
-// Send Style, do not change
-app.get("/style.css", function (req, res) {
-    //Feel free to change the contents of style.css to prettify your Web app
-    res.sendFile(path.join(__dirname + "/public/css/style.css"));
-});
-
-// Send obfuscated JS, do not change
-app.get("/index.js", function(req, res) {
-	fs.readFile(path.join(__dirname + "/public/index.js"), "utf8", function(err, contents) {
-		const minimizedContents = JavaScriptObfuscator.obfuscate(contents, { compact: true, controlFlowFlattening: true });
-		res.contentType("application/javascript");
-		res.send(minimizedContents._obfuscatedCode);
-	});
-});
-
-app.use('/img', express.static(__dirname + '/public/img'));
-
-app.get("/signIn/:user_email/:user_password", function (req, res) {
-    var email = req.params.user_email;
-    var password = req.params.user_password
-    res.send(`Sucessfull Sign In\nEmail: ${email}\nPassword: ${password}`);
-});
-
-//Respond to POST requests that upload files to uploads/ directory
-app.post("/upload", function(req, res) {
-	if (!req.files) {
-		return res.status(400).send("No files were uploaded.");
-	}
-
-	let uploadFile = req.files.uploadFile;
-
-	// Use the mv() method to place the file somewhere on your server
-	uploadFile.mv("uploads/" + uploadFile.name, function(err) {
-		if (err) {
-			return res.status(500).send(err);
-		}
-		res.redirect("/");
-	});
-});
-
-//Respond to GET requests for files in the uploads/ directory
-app.get("/uploads/:name", function(req, res) {
-	fs.stat("uploads/" + req.params.name, function(err, stat) {
-		console.log(err);
-		if (err == null) {
-			res.sendFile(path.join(__dirname + "/uploads/" + req.params.name));
-		} else {
-			res.send("");
-		}
-	});
-});
-
-app.listen(portNum);
-console.log("Running app at localhost: " + portNum);
-
-*/
--- public/index.js
+++ public/index.js
@@ -1,43 +1,18 @@
 docReady(function () {
-    /* var signInButton = document.getElementsByClassName("sign-in-module__btn");
-    signInButton[0].addEventListener("click", function(e){
-        e.preventDefault();
-        var user_email = document.getElementsByClassName("sign-in-module__form--email")[0].value;
-		var user_password = document.getElementsByClassName("sign-in-module__form--password")[0].value;
-        var request = new XMLHttpRequest();
-		request.open("GET", `/signIn/${user_email}/${user_password}`, true);
-
-		request.onload = function () {
-			if (this.status >= 200 && this.status < 400) {
-                var resp = this.response;
-                console.log(`${resp}`);
-			} else {
-                // We reached our target server, but it returned an error
-                console.log("Error");
-			}
-		};
-
-		request.onerror = function () {
-            // There was a connection error of some sort
-            console.log("Connection Error");
-		};
-        request.send();
-    }
-    ,false); */
 
     var signInButton = document.getElementsByClassName("sign-in-module__btn");
 
     signInButton[0].addEventListener("click", function(e){
         e.preventDefault();
 
-        var user_email = document.getElementsByClassName("sign-in-module__form--email")[0].value;
+        var user_username = document.getElementsByClassName("sign-in-module__form--username")[0].value;
         var user_password = document.getElementsByClassName("sign-in-module__form--password")[0].value;
-        
-        console.log(`Email: ${user_email}\nPassword: ${user_password}`);
-        console.log('Data Sent\n');
 
+        var verifyResults = verifyUsernameFormat__accountRegistry(user_username);
+        console.log(verifyResults);
+        
         var request = new XMLHttpRequest();
-		request.open("GET", `/createUser/:${user_email}/:${user_password}`, true);
+		request.open("GET", `/createUser/:${user_username}/:${user_password}`, true);
 
 		request.onload = function () {
 			if (this.status >= 200 && this.status < 400) {
@@ -66,3 +41,36 @@ function docReady(fn) {
         document.addEventListener("DOMContentLoaded", fn);
     }
 }    
+
+function verifyUsernameFormat__accountRegistry(username) {
+	//(username.match(/[|\\/~^:,;?!&%$@*+]/))
+	/* Username Rules:
+    - length 1-16
+    - Characters: 0 - 9, a - z, A - z
+    - No special characters, symbols or spaces
+*/
+
+	if (/\s/.test(username)) {
+		return "error";
+	}
+	if (/[^a-zA-Z0-9]/.test(username)) {
+		return "symbol";
+	}
+
+	function isAlphaNumeric(str) {
+		var code, i, len;
+
+		for (i = 0, len = str.length; i < len; i++) {
+			code = str.charCodeAt(i);
+			if (
+				!(code > 47 && code < 58) && // numeric (0-9)
+				!(code > 64 && code < 91) && // upper alpha (A-Z)
+				!(code > 96 && code < 123)
+			) {
+				// lower alpha (a-z)
+				return false;
+			}
+		}
+		return true;
+	}
+}
